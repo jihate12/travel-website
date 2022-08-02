9975237a766e3b54ab87a2dd9763e102091e89ee
Arief <fffeirarief12@gmail.com>
2022-08-02 17:22:03 +0700

    Updates

---
+++ precache-manifest.5f4ee61c30de53cc8b08cad4058d49f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e31c4dc7e2076ff053f",
+    "url": "/travel-website-react/static/css/main.7386ba77.chunk.css"
+  },
+  {
+    "revision": "9e31c4dc7e2076ff053f",
+    "url": "/travel-website-react/static/js/main.9e31c4dc.chunk.js"
+  },
+  {
+    "revision": "c7fe8dbc2a767bc0d2d1",
+    "url": "/travel-website-react/static/js/1.c7fe8dbc.chunk.js"
+  },
+  {
+    "revision": "30625baa164f5f18deab",
+    "url": "/travel-website-react/static/js/2.30625baa.chunk.js"
+  },
+  {
+    "revision": "23f57397a643e41be6cc",
+    "url": "/travel-website-react/static/js/runtime~main.23f57397.js"
+  },
+  {
+    "revision": "c2b7f951e0d54e6cc624445b3610eb4b",
+    "url": "/travel-website-react/index.html"
+  }
+];
